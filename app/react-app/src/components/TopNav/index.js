d2013b851897690662155c951a649e1710d76892
Jessica Valarezo <jgv2108@columbia.edu>
2017-04-12 21:23:56 -0700

    temp

--- app/react-app/src/components/TopNav/index.js
+++ app/react-app/src/components/TopNav/index.js
@@ -1,7 +1,11 @@
 import React, { Component, PropTypes } from 'react'
-import { isActive } from '../reducers'
+import { connect } from 'react-redux'
+import { isActive } from '../../reducers'
 import LoginForm from '../LoginForm'
 import CreateUserForm from '../CreateUserForm'
+import FlatButton from 'material-ui/FlatButton'
+import './styles.css'
+
 
 class TopNav extends Component {
     loginUser() {
@@ -21,13 +25,18 @@ class TopNav extends Component {
     }
 
     renderInactive() {
+        /*<LoginForm handleSubmit={this.loginUser()}/>
+        <CreateUserForm handleSubmit={this.createUser()}/>*/
         return (
-            <div>
-                <LoginForm handleSubmit={this.loginUser()}/>
-                <CreatUserForm handleSubmit={this.createUser()}/>
+            <div className='navContainer'>
+            <div className='buttonSection'>
+                <FlatButton label="Create User" />
+                <FlatButton label="Login" />
+            </div>
             </div>
         )
     }
+
     render() {
         const { isActive } = this.props;
         return isActive? this.renderActive() : this.renderInactive();
--- app/react-app/src/containers/App.js
+++ app/react-app/src/containers/App.js
@@ -1,21 +1,16 @@
 import React from 'react'
 import GradientBackground from '../components/GradientBackground'
-import Header from '../components/Header'
-import ProductsContainer from './ProductsContainer'
-import CustomerContainer from './CustomerContainer'
-import LoginContainer from './LoginContainer'
-import CartContainer from './CartContainer'
-import LoginForm from '../components/LoginForm'
 import TopNav from '../components/TopNav'
+import Header from '../components/Header'
+import TitleContainer from './TitleContainer'
+import ProductsContainer from './ProductsContainer'
 
 const App = () => (
   <div>
-    <TopNav />
     <GradientBackground />
-    <LoginContainer />
-    <CustomerContainer />
+    <TopNav />
     <Header />
-    <CartContainer />
+    <TitleContainer />
     <ProductsContainer />
   </div>
 )
